049d3aad572edb28bb5a83597a8184f955053441
Marcos Paulo Calera <caleramp@gmail.com>
2025-08-19 17:10:13 -0300

    outputting task data in the task component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,16 @@
 import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
+// type User = {
+//   id: string; 
+//   name: string; 
+//   avatar: string;
+// }
 
-// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -12,34 +20,20 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // selectedUser = signal(DUMMY_USERS[randomIndex]); // i will use signals only here in this code to train all angular concepts
-  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar
-  // }
 
   // when working with signals inputs, you get this advantgage that angular manages these behind the scenes
   // subscriptions and updates for you pparts of UI or the values in you code, can it be more efficient 
   // so, decorators are older than signals, in evidenz i will probably use signals
-   
-  // avatar = input.required<string>(); // signals
-  // name = input.required<string>();
 
-  // imagePath = computed(() => {
-  //   return 'assets/users/' + this.avatar(); // signals
-  // })
+  @Input({ required: true }) user!: User; 
 
-  @Input({required: true}) id!: string; // decorator
-  @Input({required: true}) avatar!: string; // decorator
-  @Input({required: true}) name!: string;
   @Output() select = new EventEmitter<string>(); 
-  //select = output<string>(); // signal
 
   get imagePath() {
-    return 'assets/users/' + this.avatar; // decorator
+    return 'assets/users/' + this.user.avatar; 
   }
 
   onSelectUser() {
-    this.select.emit(this.id); 
+    this.select.emit(this.user.id); 
   }
 }
